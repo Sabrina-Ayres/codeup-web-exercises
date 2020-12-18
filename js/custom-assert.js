143dd2b51fc3fa36dc011022cc5970bb40782f20
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2020-12-18 08:06:12 -0600

    adding files

---
+++ js/custom-assert.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function testFunction(parameter){
+    if (parameter){
+       if (function returnInputAsOutput(input){
+        return input === input;
+       ) return true;
+     else {
+        return false;
+    }
+}
