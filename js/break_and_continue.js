e5087027bb625bb9ab6be26b54ee9be086010517
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2020-12-24 11:37:47 -0600

    Completed break and continue exercie

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,29 @@
 "use strict";
 
-var oddNumber = prompt("Enter an odd number between 1 and 50...");
+function isValid(input) {
+    input = parseFloat(input);
+    var isNum = !isNaN(input);
+    var isOdd = input % 2 !== 0;
+    var isInRange = input >= 1 && input <= 50;
+    return isNum && isOdd && isInRange;
+}
 
-for (var i = 1; i < 50; i++) {
+var userNumber;
+while(true) {
+    userNumber = prompt("Enter an odd number between 1 and 50...");
+    if (isValid(userNumber)){
+        break;
+    }
+}
 
-    if (oddNumber % 2 !== 0) {
+console.log("Number to skip is: " + userNumber);
+
+for (var i = 1; i <= 50; i++) {
+    if (i == userNumber){
+        console.log("Yikes! Skipping number: " + userNumber);
+        continue;
+    }
+    if (i %2 === 0){
         continue;
     }
     console.log("Here is an odd number: " + i);
