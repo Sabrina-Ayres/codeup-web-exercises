c8e8f389d02b388d56e4e49f9b36b5b342b8c041
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2020-12-27 12:51:17 -0600

    Completed while loop exercise

--- js/while.js
+++ js/while.js
@@ -1,1 +1,26 @@
 "use strict";
+
+var num = 2;
+while (num <= 65536){
+    console.log(num);
+    num *= 2;
+}
+
+//////////////////////
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var conesLeft;
+console.log("There are " + allCones + " cones to sell today.");
+
+do {
+    conesLeft = Math.floor(Math.random() * 5) + 1;
+    if (conesLeft <= allCones) {
+        console.log("Number of cones sold: " + conesLeft);
+        allCones -= conesLeft;
+    } else {
+        console.log("I cannot sell you " + conesLeft + ". I only have " + allCones + " left...");
+    }
+    console.log(allCones + " are left...");
+} while(allCones > 0);
+
+console.log("YAY! I sold them all!");
