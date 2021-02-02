41e02abf59ae49ac77189dbfa946037d2feba3dc
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-02-02 14:46:39 -0600

    selectors exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,4 +1,16 @@
 
-$(document).ready(function () {
-    alert("The DOM has finished loading.")
-})
+// $(document).ready(function () {
+//     alert("The DOM has finished loading.")
+// })
+//
+// var contents = $('#mid').html();
+// alert(contents)
+
+// $('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px');
+
+var contents = $('h1').html();
+alert(contents)
+
+$('h1, p, li').css('background-color', 'pink');
