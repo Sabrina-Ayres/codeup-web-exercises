5cbb43986901f7f57a2d839399fd6ea4b305e584
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-02-03 10:18:50 -0600

    events exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -8,9 +8,31 @@
 
 // $('.codeup').css('border', '1px solid red');
 
-$('li').css('font-size', '20px');
+// $('li').css('font-size', '20px');
+//
+// var contents = $('h1').html();
+// alert(contents)
+//
+// $('h1, p, li').css('background-color', 'pink');
 
-var contents = $('h1').html();
-alert(contents)
 
-$('h1, p, li').css('background-color', 'pink');
+let changeTitle = $('.title');
+    let handler = function(event){
+        $(this).css('background-color', 'pink');
+    }
+    changeTitle.click(handler);
+
+
+let changeFont = $('p');
+    let handle = function(e) {
+        $(this).css('font-size', '18px');
+    }
+    changeFont.dblclick(handle);
+
+
+let changeLine = $('li');
+    let mouse = function(e) {
+        changeLine.css('color', '');
+        $(this).css('color', 'red')
+    }
+    changeLine.hover(mouse);
